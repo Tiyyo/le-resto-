5e24519fcef9bac823ebc010f5603fe77d12c711
Tiyyo <111092370+Tiyyo@users.noreply.github.com>
2022-09-11 21:06:18 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+
+// activation menu burger 
+
+function activeMenu (){
 const icBurger = document.querySelector(".hamburger-ic");
 const navLinks = document.querySelector(".main-navbar");
+const burgerMenu =  document.querySelector(".bar")
 
-
-
-icBurger.addEventListener('click',(e) => {  
+icBurger.addEventListener('click',() => {  
     navLinks.classList.toggle('active')
-    navLinks.classList.remove('main-navbar')
-    
+    burgerMenu.classList.toggle('remove_bar')
 })
-
+}
+activeMenu();
